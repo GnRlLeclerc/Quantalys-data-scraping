aeb1009df784e24ff32bc261670626c9604f1775
Thibaut <thibaut2saivre@gmail.com>
2025-03-14 13:43:53 +0100

    Base pour la version webapp

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new HtmlInlineScriptPlugin(),
+  ],
+};
